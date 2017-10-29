8b907d0521cb9b90326c4d2164e078f49c630cad
Matthew LaSpina <mlaspina519@gmail.com>
2017-10-29 14:15:11 -0400

    Add/Remove air, and "tire gauge" type of button

---
+++ Assets/Scripts/ChangeAirPressure.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeAirPressure : MonoBehaviour {
+    public Text airPressure;
+    public GameObject air;
+    public GameObject airText;
+
+    public void AddAir() {
+        airText.SetActive(false);
+        int num = System.Convert.ToInt32(air.GetComponent<Text>().text);
+        num++;
+        airPressure.text = num.ToString();
+    }
+
+    public void RemoveAir() {
+        airText.SetActive(false);
+        int num = System.Convert.ToInt32(air.GetComponent<Text>().text);
+        num--;
+        airPressure.text = num.ToString();
+    }
+}
---
+++ Assets/Scripts/DisplayValue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayValue : MonoBehaviour {
+    public GameObject num;
+
+    public void OnButtonClick() {
+        num.SetActive(true);
+    }
+}
