05a0bf8a1a5ddad2d9cf6c6c3236bc3b314ac4f2
Matthew LaSpina <mlaspina519@gmail.com>
2017-10-29 14:47:40 -0400

    Map Scene - 2d sprites added, will be used for collision/animation/scene transitions

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private float speed = 2.0f;
+    private float horizontalMovement;
+    private float verticalMovement;
+    private Rigidbody2D rigid;
+    private bool facingLeft = true;
+    
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+	}
+
+    void FixedUpdate() {
+        rigid.velocity = new Vector2(verticalMovement * speed, rigid.velocity.y);
+        rigid.velocity = new Vector2(horizontalMovement * speed, rigid.velocity.x);
+
+        // Rotates car to face left/right
+        if(horizontalMovement > 0 && facingLeft || horizontalMovement < 0 && !facingLeft) {
+            transform.localScale = new Vector2(transform.localScale.x, -transform.localScale.y);
+            facingLeft = !facingLeft;
+        }
+        
+    }
+}
