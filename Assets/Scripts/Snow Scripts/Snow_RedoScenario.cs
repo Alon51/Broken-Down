06a368bf8a6592b2e105959f83a687ba564d447f
KNIGHT-BARON\forev <foreverproud@hotmail.com>
2017-12-02 19:13:37 -0500

    Added functionality to snow scenario. It's nearly complete. Modified text in LowTireScene

---
+++ Assets/Scripts/Snow Scripts/Snow_RedoScenario.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow_RedoScenario : MonoBehaviour {
+
+    public GameObject scenarioCanvas;
+    public GameObject incorrectCanvas;
+
+    public void OnButtonClick()
+    {
+        scenarioCanvas.SetActive(true);
+        incorrectCanvas.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Snow Scripts/Snow_Toggles.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Snow_Toggles : MonoBehaviour {
+
+    public Toggle traction;
+    public Toggle shovel;
+    public Toggle dirt;
+    public Toggle tire;
+    public Toggle pedal;
+  
+
+    public GameObject snowCorrectCanvas;
+    public GameObject snowIncorrectCanvas;
+    public GameObject scenarioCanvas;
+
+    public void OnButtonClick()
+    {
+        int sequence = 0;
+
+        if (traction.isOn)
+        {
+            sequence++;
+        }
+
+        if (traction.isOn && shovel.isOn)
+        {
+            sequence++; ;
+        }
+
+        if (traction.isOn && shovel.isOn && dirt.isOn)
+        {
+            sequence++;
+        }
+
+        if (traction.isOn && shovel.isOn && dirt.isOn && tire.isOn)
+        {
+            sequence++;
+        }
+
+        if (traction.isOn && shovel.isOn && dirt.isOn && tire.isOn && pedal.isOn)
+        {
+            sequence++;
+        }
+
+        else {
+
+            sequence = 0;
+
+            traction.isOn = false;
+            shovel.isOn = false;
+            dirt.isOn = false;
+            tire.isOn = false;
+            pedal.isOn = false;
+
+        }
+
+
+
+        if (sequence == 5)
+        {
+            scenarioCanvas.SetActive(false);
+            snowCorrectCanvas.SetActive(true);
+        }
+        else
+        {
+            scenarioCanvas.SetActive(false);
+            snowIncorrectCanvas.SetActive(true);
+            sequence = 0;
+        }
+    }
+}
