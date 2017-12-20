667b7c45d7206343fcccadcede78530be8476690
Alon Butbul <alon51b@gmail.com>
2017-12-19 23:49:53 -0500

    Last scene is ready

---
+++ Assets/submitButton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class submitButton : MonoBehaviour {
+
+	public GameObject showInfoPanel;
+	public Button infoButton;
+	public Dropdown one;
+	public Dropdown two;
+	public Dropdown three;
+	public Dropdown four;
+	byte triels;
+	public Text text;
+
+	void Start()
+	{
+		triels = 0;
+		showInfoPanel.SetActive(false);
+
+		Button btn = infoButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		triels++;
+		if (triels == 3) {
+			text.text = "Set option one to: Will try to remain calm to be able to think clearly";
+		}else if(triels == 5){
+			text.text = "Set option two to: Find a safe zone to rest while I am trying to manage the situation";
+		}else if(triels == 6){
+			text.text = "Set option three to: If there are injured people I will call 911";
+		}else if(triels == 7){
+			text.text = "Set option four to: Let my insurance company know about the accident";
+		}
+			
+		if(one.captionText.text.Equals("Will try to remain calm to be able to think clearly")){
+			
+			if (two.captionText.text.Equals ("Find a safe zone to rest while I am trying to manage the situation")) {
+				
+				if (three.captionText.text.Equals ("If there are injured people I will call 911")) {
+					
+					if (four.captionText.text.Equals ("Let my insurance company know about the accident")) {
+						
+						showInfoPanel.gameObject.SetActive (true);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/whatToDoScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class whatToDoScript : MonoBehaviour {
+
+	public GameObject showInfoPanel;
+	public Button infoButton;
+
+	void Start()
+	{
+		showInfoPanel.SetActive(false);
+
+		Button btn = infoButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		if (showInfoPanel.gameObject.activeInHierarchy.Equals(false)) {
+			
+			showInfoPanel.gameObject.SetActive (true);
+		}
+	}
+}
